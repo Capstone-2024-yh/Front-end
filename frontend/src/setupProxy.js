55125e47d7e75906ffe35728fbdac56761964996
CloudNru <grutugy1025@gmail.com>
2024-10-12 23:17:50 +0900

    Solve Cors by proxy

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/v2/local', // 프록시 경로 설정
+        createProxyMiddleware({
+            target: 'https://dapi.kakao.com', // 요청을 보낼 타겟 URL (Kakao API 서버)
+            changeOrigin: true, // 원본 서버와 다른 도메인으로 요청할 수 있도록 설정
+            pathRewrite: {
+                '^/v2/local': '/v2/local', // 프록시 경로 리라이트
+            },
+        })
+    );
+};
