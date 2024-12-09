1c1cc73bb98405155c84bb8eee2a2a1cd57ae7b4
Richardyun01 <richardyun01@gmail.com>
2024-12-09 18:53:08 +0900

    Connection Test

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   // baseURL: 'http://localhost:8080',
-  withCredentials: true,  // 쿠키 기반 인증을 위한 옵션
+  withCredentials: false,  // 쿠키 기반 인증을 위한 옵션
 });
 
 export default instance;
