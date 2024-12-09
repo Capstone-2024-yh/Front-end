278849cd6052d833fc303f6928d9c7c20637ab79
Richardyun01 <richardyun01@gmail.com>
2024-12-09 17:39:46 +0900

    Connection Test

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -11,4 +11,15 @@ module.exports = function (app) {
             },
         })
     );
+
+    app.use(
+        '/search-summary', // 프록시 경로 설정
+        createProxyMiddleware({
+            target: 'https://capstone.newdongjun.com/', // 요청을 보낼 타겟 URL
+            changeOrigin: true, // 원본 서버와 다른 도메인으로 요청할 수 있도록 설정
+            pathRewrite: {
+                '^/search-summary': '/search-summary', // 프록시 경로 리라이트
+            },
+        })
+    );
 };
